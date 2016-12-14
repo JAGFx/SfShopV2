68e0f36cba30a1a285c71cbb386e436c5ddc5e5d
JAGFx <contact@jagfx.fr>
2016-12-14 19:20:53 +0100

    Feature Commande: Fix constructor, add constants + add CRUD support

--- src/sil21/VitrineBundle/Entity/Commande.php
+++ src/sil21/VitrineBundle/Entity/Commande.php
@@ -1,156 +1,153 @@
 <?php
-
-namespace sil21\VitrineBundle\Entity;
-
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * Commande
- */
-class Commande
-{
-    /**
-     * @var integer
-     */
-    private $id;
-
-    /**
-     * @var \DateTime
-     */
-    private $date;
-
-    /**
-     * @var boolean
-     */
-    private $etat;
-
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     */
-    private $lignecommandes;
-
-    /**
-     * @var \sil21\VitrineBundle\Entity\Client
-     */
-    private $client;
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->lignecommandes = new \Doctrine\Common\Collections\ArrayCollection();
-    }
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set date
-     *
-     * @param \DateTime $date
-     * @return Commande
-     */
-    public function setDate($date)
-    {
-        $this->date = $date;
-
-        return $this;
-    }
-
-    /**
-     * Get date
-     *
-     * @return \DateTime 
-     */
-    public function getDate()
-    {
-        return $this->date;
-    }
-
-    /**
-     * Set etat
-     *
-     * @param boolean $etat
-     * @return Commande
-     */
-    public function setEtat($etat)
-    {
-        $this->etat = $etat;
-
-        return $this;
-    }
-
-    /**
-     * Get etat
-     *
-     * @return boolean 
-     */
-    public function getEtat()
-    {
-        return $this->etat;
-    }
-
-    /**
-     * Add lignecommandes
-     *
-     * @param \sil21\VitrineBundle\Entity\LigneCommande $lignecommandes
-     * @return Commande
-     */
-    public function addLignecommande(\sil21\VitrineBundle\Entity\LigneCommande $lignecommandes)
-    {
-        $this->lignecommandes[] = $lignecommandes;
-
-        return $this;
-    }
-
-    /**
-     * Remove lignecommandes
-     *
-     * @param \sil21\VitrineBundle\Entity\LigneCommande $lignecommandes
-     */
-    public function removeLignecommande(\sil21\VitrineBundle\Entity\LigneCommande $lignecommandes)
-    {
-        $this->lignecommandes->removeElement($lignecommandes);
-    }
-
-    /**
-     * Get lignecommandes
-     *
-     * @return \Doctrine\Common\Collections\Collection 
-     */
-    public function getLignecommandes()
-    {
-        return $this->lignecommandes;
-    }
-
-    /**
-     * Set client
-     *
-     * @param \sil21\VitrineBundle\Entity\Client $client
-     * @return Commande
-     */
-    public function setClient(\sil21\VitrineBundle\Entity\Client $client = null)
-    {
-        $this->client = $client;
-
-        return $this;
-    }
-
-    /**
-     * Get client
-     *
-     * @return \sil21\VitrineBundle\Entity\Client 
-     */
-    public function getClient()
-    {
-        return $this->client;
-    }
-}
+	
+	namespace sil21\VitrineBundle\Entity;
+	
+	use Doctrine\ORM\Mapping as ORM;
+	
+	/**
+	 * Commande
+	 */
+	class Commande {
+		const CMD_NOT_VALIDATE = 0;
+		const CMD_VALIDATE     = 1;
+		
+		/**
+		 * @var integer
+		 */
+		private $id;
+		
+		/**
+		 * @var \DateTime
+		 */
+		private $date;
+		
+		/**
+		 * @var integer
+		 */
+		private $etat = self::CMD_NOT_VALIDATE;
+		
+		/**
+		 * @var \Doctrine\Common\Collections\Collection
+		 */
+		private $lignecommandes;
+		
+		/**
+		 * @var \sil21\VitrineBundle\Entity\Client
+		 */
+		private $client;
+		
+		/**
+		 * Constructor
+		 */
+		public function __construct( Client $client ) {
+			$this->setClient( $client );
+			$this->date = new \DateTime();
+			$this->lignecommandes = new \Doctrine\Common\Collections\ArrayCollection();
+		}
+		
+		/**
+		 * Get id
+		 *
+		 * @return integer
+		 */
+		public function getId() {
+			return $this->id;
+		}
+		
+		/**
+		 * Set date
+		 *
+		 * @param \DateTime $date
+		 *
+		 * @return Commande
+		 */
+		public function setDate( $date ) {
+			$this->date = $date;
+			
+			return $this;
+		}
+		
+		/**
+		 * Get date
+		 *
+		 * @return \DateTime
+		 */
+		public function getDate() {
+			return $this->date;
+		}
+		
+		/**
+		 * Set etat
+		 *
+		 * @param integer $etat
+		 *
+		 * @return Commande
+		 */
+		public function setEtat( $etat ) {
+			$this->etat = $etat;
+			
+			return $this;
+		}
+		
+		/**
+		 * Get etat
+		 *
+		 * @return integer
+		 */
+		public function getEtat() {
+			return $this->etat;
+		}
+		
+		/**
+		 * Add lignecommandes
+		 *
+		 * @param \sil21\VitrineBundle\Entity\LigneCommande $lignecommandes
+		 *
+		 * @return Commande
+		 */
+		public function addLignecommande( \sil21\VitrineBundle\Entity\LigneCommande $lignecommandes ) {
+			$this->lignecommandes[] = $lignecommandes;
+			
+			return $this;
+		}
+		
+		/**
+		 * Remove lignecommandes
+		 *
+		 * @param \sil21\VitrineBundle\Entity\LigneCommande $lignecommandes
+		 */
+		public function removeLignecommande( \sil21\VitrineBundle\Entity\LigneCommande $lignecommandes ) {
+			$this->lignecommandes->removeElement( $lignecommandes );
+		}
+		
+		/**
+		 * Get lignecommandes
+		 *
+		 * @return \Doctrine\Common\Collections\Collection
+		 */
+		public function getLignecommandes() {
+			return $this->lignecommandes;
+		}
+		
+		/**
+		 * Set client
+		 *
+		 * @param \sil21\VitrineBundle\Entity\Client $client
+		 *
+		 * @return Commande
+		 */
+		public function setClient( \sil21\VitrineBundle\Entity\Client $client = null ) {
+			$this->client = $client;
+			
+			return $this;
+		}
+		
+		/**
+		 * Get client
+		 *
+		 * @return \sil21\VitrineBundle\Entity\Client
+		 */
+		public function getClient() {
+			return $this->client;
+		}
+	}
--- src/sil21/VitrineBundle/Entity/LigneCommande.php
+++ src/sil21/VitrineBundle/Entity/LigneCommande.php
@@ -1,124 +1,114 @@
 <?php
-
-namespace sil21\VitrineBundle\Entity;
-
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * LigneCommande
- */
-class LigneCommande
-{
-    /**
-     * @var integer
-     */
-    private $qte;
-
-    /**
-     * @var float
-     */
-    private $prix;
-
-    /**
-     * @var \sil21\VitrineBundle\Entity\Product
-     */
-    private $product;
-
-    /**
-     * @var \sil21\VitrineBundle\Entity\Commande
-     */
-    private $commande;
-
-
-    /**
-     * Set qte
-     *
-     * @param integer $qte
-     * @return LigneCommande
-     */
-    public function setQte($qte)
-    {
-        $this->qte = $qte;
-
-        return $this;
-    }
-
-    /**
-     * Get qte
-     *
-     * @return integer 
-     */
-    public function getQte()
-    {
-        return $this->qte;
-    }
-
-    /**
-     * Set prix
-     *
-     * @param float $prix
-     * @return LigneCommande
-     */
-    public function setPrix($prix)
-    {
-        $this->prix = $prix;
-
-        return $this;
-    }
-
-    /**
-     * Get prix
-     *
-     * @return float 
-     */
-    public function getPrix()
-    {
-        return $this->prix;
-    }
-
-    /**
-     * Set product
-     *
-     * @param \sil21\VitrineBundle\Entity\Product $product
-     * @return LigneCommande
-     */
-    public function setProduct(\sil21\VitrineBundle\Entity\Product $product)
-    {
-        $this->product = $product;
-
-        return $this;
-    }
-
-    /**
-     * Get product
-     *
-     * @return \sil21\VitrineBundle\Entity\Product 
-     */
-    public function getProduct()
-    {
-        return $this->product;
-    }
-
-    /**
-     * Set commande
-     *
-     * @param \sil21\VitrineBundle\Entity\Commande $commande
-     * @return LigneCommande
-     */
-    public function setCommande(\sil21\VitrineBundle\Entity\Commande $commande)
-    {
-        $this->commande = $commande;
-
-        return $this;
-    }
-
-    /**
-     * Get commande
-     *
-     * @return \sil21\VitrineBundle\Entity\Commande 
-     */
-    public function getCommande()
-    {
-        return $this->commande;
-    }
-}
+	
+	namespace sil21\VitrineBundle\Entity;
+	
+	use Doctrine\ORM\Mapping as ORM;
+	
+	/**
+	 * LigneCommande
+	 */
+	class LigneCommande {
+		/**
+		 * @var integer
+		 */
+		private $qte;
+		
+		/**
+		 * @var \sil21\VitrineBundle\Entity\Product
+		 */
+		private $product;
+		
+		/**
+		 * @var \sil21\VitrineBundle\Entity\Commande
+		 */
+		private $commande;
+		
+		/**
+		 * LigneCommande constructor.
+		 *
+		 * @param Product  $product
+		 * @param Commande $commande
+		 * @param integer  $qte
+		 */
+		public function __construct( Product $product, Commande $commande, $qte = 1 ) {
+			$this->qte = $qte;
+			$this->product = $product;
+			$this->commande = $commande;
+		}
+		
+		
+		/**
+		 * Set qte
+		 *
+		 * @param integer $qte
+		 *
+		 * @return LigneCommande
+		 */
+		public function setQte( $qte ) {
+			$this->qte = $qte;
+			
+			return $this;
+		}
+		
+		/**
+		 * Get qte
+		 *
+		 * @return integer
+		 */
+		public function getQte() {
+			return $this->qte;
+		}
+		
+		/**
+		 * Get prix
+		 *
+		 * @return float
+		 */
+		public function getPrix() {
+			return $this->qte * $this->product->getPrice();
+		}
+		
+		/**
+		 * Set product
+		 *
+		 * @param \sil21\VitrineBundle\Entity\Product $product
+		 *
+		 * @return LigneCommande
+		 */
+		public function setProduct( \sil21\VitrineBundle\Entity\Product $product ) {
+			$this->product = $product;
+			
+			return $this;
+		}
+		
+		/**
+		 * Get product
+		 *
+		 * @return \sil21\VitrineBundle\Entity\Product
+		 */
+		public function getProduct() {
+			return $this->product;
+		}
+		
+		/**
+		 * Set Commande
+		 *
+		 * @param \sil21\VitrineBundle\Entity\Commande $commande
+		 *
+		 * @return LigneCommande
+		 */
+		public function setCommande( \sil21\VitrineBundle\Entity\Commande $commande ) {
+			$this->commande = $commande;
+			
+			return $this;
+		}
+		
+		/**
+		 * Get Commande
+		 *
+		 * @return \sil21\VitrineBundle\Entity\Commande
+		 */
+		public function getCommande() {
+			return $this->commande;
+		}
+	}
