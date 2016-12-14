9c9698e6e4beef9231134d6e90a54ad9909506f8
JAGFx <contact@jagfx.fr>
2016-12-14 19:18:06 +0100

    Feature Category: Add CRUD support + New rout for list all

--- src/sil21/VitrineBundle/Controller/CategoryController.php
+++ src/sil21/VitrineBundle/Controller/CategoryController.php
@@ -4,13 +4,18 @@
 	
 	use sil21\VitrineBundle\Entity\Category;
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\HttpFoundation\Request;
 	
 	class CategoryController extends Controller {
 		public function indexAction() {
-			$categories = $this->getDoctrine()->getRepository( 'sil21VitrineBundle:Category' )->findAll();
+			$em = $this->getDoctrine()->getManager();
+			
+			$categories = $em->getRepository( 'sil21VitrineBundle:Category' )->findAll();
 			
 			return $this->render(
-				'listCategories.html.twig', [ 'categories' => $categories ]
+				'sil21VitrineBundle:Category:index.html.twig', [
+					'categories' => $categories,
+				]
 			);
 		}
 		
@@ -23,7 +28,8 @@
 			return $this->render(
 				'sil21VitrineBundle:Catalogue:listProducts.html.twig',
 				[
-					'filter' => $category
+					'name' => $category->getName(),
+					'products' => $category->getProducts()
 				]
 			);
 		}
@@ -34,5 +40,100 @@
 			return $this->render(
 				'sil21VitrineBundle:Category:listCategories.html.twig', [ 'categories' => $categories ]
 			);
+		}
+		
+		/**
+		 * Creates a new category entity.
+		 *
+		 */
+		public function newAction( Request $request ) {
+			$category = new Category();
+			$form = $this->createForm( 'sil21\VitrineBundle\Form\CategoryType', $category );
+			$form->handleRequest( $request );
+			
+			if ( $form->isSubmitted() && $form->isValid() ) {
+				$em = $this->getDoctrine()->getManager();
+				$em->persist( $category );
+				$em->flush( $category );
+				
+				return $this->redirectToRoute( 'category_show', [ 'id' => $category->getId() ] );
+			}
+			
+			return $this->render(
+				'sil21VitrineBundle:Category:new.html.twig', [
+					'category' => $category,
+					'form'     => $form->createView(),
+				]
+			);
+		}
+		
+		/**
+		 * Finds and displays a category entity.
+		 *
+		 */
+		public function showAction( Category $category ) {
+			$deleteForm = $this->createDeleteForm( $category );
+			
+			return $this->render(
+				'sil21VitrineBundle:Category:show.html.twig', [
+					'category'    => $category,
+					'delete_form' => $deleteForm->createView(),
+				]
+			);
+		}
+		
+		/**
+		 * Displays a form to edit an existing category entity.
+		 *
+		 */
+		public function editAction( Request $request, Category $category ) {
+			$deleteForm = $this->createDeleteForm( $category );
+			$editForm = $this->createForm( 'sil21\VitrineBundle\Form\CategoryType', $category );
+			$editForm->handleRequest( $request );
+			
+			if ( $editForm->isSubmitted() && $editForm->isValid() ) {
+				$this->getDoctrine()->getManager()->flush();
+				
+				return $this->redirectToRoute( 'category_edit', [ 'id' => $category->getId() ] );
+			}
+			
+			return $this->render(
+				'sil21VitrineBundle:Category:edit.html.twig', [
+					'category'    => $category,
+					'edit_form'   => $editForm->createView(),
+					'delete_form' => $deleteForm->createView(),
+				]
+			);
+		}
+		
+		/**
+		 * Deletes a category entity.
+		 *
+		 */
+		public function deleteAction( Request $request, Category $category ) {
+			$form = $this->createDeleteForm( $category );
+			$form->handleRequest( $request );
+			
+			if ( $form->isSubmitted() && $form->isValid() ) {
+				$em = $this->getDoctrine()->getManager();
+				$em->remove( $category );
+				$em->flush( $category );
+			}
+			
+			return $this->redirectToRoute( 'category_index' );
+		}
+		
+		/**
+		 * Creates a form to delete a category entity.
+		 *
+		 * @param Category $category The category entity
+		 *
+		 * @return \Symfony\Component\Form\Form The form
+		 */
+		private function createDeleteForm( Category $category ) {
+			return $this->createFormBuilder()
+				->setAction( $this->generateUrl( 'category_delete', [ 'id' => $category->getId() ] ) )
+				->setMethod( 'DELETE' )
+				->getForm();
 		}
 	}
