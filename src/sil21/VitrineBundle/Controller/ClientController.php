c25d4e1cc5b4f1eeb930048f0bfc5daa53022213
JAGFx <contact@jagfx.fr>
2016-12-16 21:05:39 +0100

    Feature Client: Add admin action list

--- src/sil21/VitrineBundle/Controller/ClientController.php
+++ src/sil21/VitrineBundle/Controller/ClientController.php
@@ -2,10 +2,16 @@
 	
 	namespace sil21\VitrineBundle\Controller;
 	
-	use FOS\UserBundle\Controller\RegistrationController as BaseController;
+	use FOS\UserBundle\Controller\ProfileController as BaseController;
 	
 	class ClientController extends BaseController {
-		public function indexAction( $name ) {
-			return $this->render( '', [ 'name' => $name ] );
+		public function indexAction() {
+			$userManager = $this->container->get( 'fos_user.user_manager' );
+			$listClients = $userManager->findUsers();
+			
+			return $this->container->get( 'templating' )->renderResponse(
+				'sil21VitrineBundle:Client:list.html.twig',
+				[ 'listClients' => $listClients ]
+			);
 		}
 	}
